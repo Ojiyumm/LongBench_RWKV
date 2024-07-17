8f5ecad6561bddd25add332a96c812b91899f35d
Ojiyumm <125381866+Ojiyumm@users.noreply.github.com>
2024-07-17 11:01:07 +0800

    Add files via upload

---
+++ LongBench-rwkv/rwkv_pred.py
@@ -0,0 +1,110 @@
+import os
+import torch
+import json
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from tqdm import tqdm
+import numpy as np
+import random
+import argparse
+
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch.nn import functional as F
+
+os.environ["RWKV_JIT_ON"] = '1'
+os.environ["RWKV_CUDA_ON"] = '1'
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default=None)
+    parser.add_argument('--e', action='store_true', help="Evaluate on LongBench-E")
+    return parser.parse_args(args)
+
+
+def get_pred(rank,  data, max_length, max_gen, prompt_format, model_name,model2path, out_path):
+    model, tokenizer = load_model_and_tokenizer(model2path[model_name])
+    model.to(device)
+    for json_obj in tqdm(data):
+        prompt = prompt_format.format(**json_obj)
+        tokenized_prompt = tokenizer(prompt, truncation=True, return_tensors="pt").input_ids[0]
+        if len(tokenized_prompt) > max_length:
+            half = int(max_length / 2)
+            prompt = tokenizer.decode(tokenized_prompt[:half], skip_special_tokens=True) + tokenizer.decode(tokenized_prompt[-half:], skip_special_tokens=True)
+        input = tokenizer(prompt, truncation=True, return_tensors="pt").to(device)
+        input = input.to(device)
+        context_length = input.input_ids.shape[-1]
+        output = model.generate(
+            **input,
+            max_new_tokens=max_gen,
+            num_beams=1,
+            do_sample=False,
+            temperature=1.0,
+        )[0]
+        pred = tokenizer.decode(output[context_length:], skip_special_tokens=True)
+        #pred = post_process(pred, model_name)
+        with open(out_path, "a", encoding="utf-8") as f:
+            json.dump({"pred": pred, "answers": json_obj["answers"], "all_classes": json_obj["all_classes"], "length": json_obj["length"]}, f, ensure_ascii=False)
+            f.write('\n')
+    dist.destroy_process_group()
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.cuda.manual_seed_all(seed)
+
+def load_model_and_tokenizer(path):
+    device=torch.device('cuda:1')
+    dtype = torch.float16
+    model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True).to(device=device, dtype=dtype)
+    tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
+    model = model.eval()
+    return model, tokenizer
+
+if __name__ == '__main__':
+    seed_everything(42)
+    args = parse_args()
+    world_size = torch.cuda.device_count()
+    mp.set_start_method('spawn', force=True)
+
+    model2path = json.load(open("config/model2path.json", "r"))
+    model2maxlen = json.load(open("config/model2maxlen.json", "r"))
+    device = torch.device('cuda:2')
+    model_name = args.model
+    max_length = model2maxlen[model_name]
+    if args.e:
+        datasets = ["qasper", "multifieldqa_en", "hotpotqa", "2wikimqa", "gov_report", "multi_news", "trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]
+    else:
+        datasets = ["samsum"]
+    dataset2prompt = json.load(open("config/dataset2prompt.json", "r"))
+    dataset2maxlen = json.load(open("config/dataset2maxlen.json", "r"))
+    if not os.path.exists("pred"):
+        os.makedirs("pred")
+    if not os.path.exists("pred_e"):
+        os.makedirs("pred_e")
+    for dataset in datasets:
+        if args.e:
+            data = load_dataset('THUDM/LongBench', f"{dataset}_e", split='test')
+            if not os.path.exists(f"pred_e/{model_name}"):
+                os.makedirs(f"pred_e/{model_name}")
+            out_path = f"pred_e/{model_name}/{dataset}.jsonl"
+        else:
+            data = load_dataset('THUDM/LongBench', dataset, split='test')
+            if not os.path.exists(f"pred/{model_name}"):
+                os.makedirs(f"pred/{model_name}")
+            out_path = f"pred/{model_name}/{dataset}.jsonl"
+        prompt_format = dataset2prompt[dataset]
+        max_gen = dataset2maxlen[dataset]
+        data_all = [data_sample for data_sample in data]
+        data_subsets = [data_all[i::world_size] for i in range(world_size)]
+        processes = []
+        for rank in range(world_size):
+            p = mp.Process(target=get_pred, args=(rank, max_length, max_gen, prompt_format, dataset, model_name, model2path, out_path))
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
