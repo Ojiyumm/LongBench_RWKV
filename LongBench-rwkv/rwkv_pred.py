aeea471ad7c1f89ff2611aed334afb08619ba4b3
Ojiyumm <125381866+Ojiyumm@users.noreply.github.com>
2024-08-05 13:36:40 +0800

    Update rwkv_pred.py

--- LongBench-rwkv/rwkv_pred.py
+++ LongBench-rwkv/rwkv_pred.py
@@ -79,7 +79,9 @@ if __name__ == '__main__':
     if args.e:
         datasets = ["qasper", "multifieldqa_en", "hotpotqa", "2wikimqa", "gov_report", "multi_news", "trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]
     else:
-        datasets = ["samsum"]
+        datasets = ["narrativeqa", "qasper", "multifieldqa_en", "multifieldqa_zh", "hotpotqa", "2wikimqa", "musique", \
+                    "dureader", "gov_report", "qmsum", "multi_news", "vcsum", "trec", "triviaqa", "samsum", "lsht", \
+                    "passage_count", "passage_retrieval_en", "passage_retrieval_zh", "lcc", "repobench-p"]
     dataset2prompt = json.load(open("config/dataset2prompt.json", "r"))
     dataset2maxlen = json.load(open("config/dataset2maxlen.json", "r"))
     if not os.path.exists("pred"):
